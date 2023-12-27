93c835b85e30eba8d0c8f38546517ebb3adc3828
alanajyang <alanayang@berkeley.edu>
2023-12-27 13:55:36 +0800

    texture buggin

---
+++ src/main/java/net/alana/modid/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.alana.modid.item;
+
+import net.alana.modid.TutorialMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item TEA = registerItem("tea", new Item(new FabricItemSettings()));
+
+    private static void addItemsToFoodAndDrinkTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(TEA);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TutorialMod.Mod_ID, name), item);
+    }
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.Mod_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToFoodAndDrinkTabItemGroup);
+    }
+}
